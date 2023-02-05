052b87899de480c8ecbab7748ecb10998f12ee2e
nadimahmed1 <nadimarafat26@gmail.com>
2023-02-05 20:11:18 +0600

    somthing card added

---
+++ src/Pages/Home/InfoCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const InfoCard = ({ img }) => {
+    return (
+        <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+            <figure><img src={img} alt="Album" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New album is released!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
