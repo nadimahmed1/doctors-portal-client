1a10c0ca7446ee58ef98c008005339b460b91386
nadimahmed1 <nadimarafat26@gmail.com>
2023-02-07 00:18:33 +0600

    code modify

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -4,26 +4,36 @@ import Appointmen from '../../assets/images/appointment.png'
 
 const Contact = () => {
     return (
-        <section style={{ background: `url(${Appointmen})` }} className='m-20 p-10'>
-            <div className='text-center'>
-                <h4 className='text-xl text-primary font-bold'>Contact Us</h4>
-                <h2 className='text-3xl text-white'>Stay connected with us..</h2>
-            </div>
 
-            <div className='text-center my-5'>
 
-                {/* <!-- xs --> */}
-                <input type="text" placeholder="Email Address" class="input input-bordered input-xs w-full max-w-xs m-2" />
+        <section className='my-10' style={{ background: `url(${Appointmen})` }}>
 
-                {/* <!-- sm --> */}
-                <br />
-                <input type="text" placeholder="Subject" class="input input-bordered input-sm w-full max-w-xs m-2" />
-                {/* <!-- md --> */}
-                <br />
+            <div class="hero min-h-screen ">
 
-                <input type="text" placeholder="Your message" class="input input-bordered input-lg w-full max-w-xs m-2" />
-                <br />
-                <PrimaryButton>Submit</PrimaryButton>
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div className='text-center'>
+                        <h4 className='text-xl text-primary font-bold'>Contact Us</h4>
+                        <h2 className='text-3xl text-white'>Stay connected with us..</h2>
+                    </div>
+
+                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl ">
+                        <div class="card-body">
+                            <div class="form-control">
+                                <input type="text" placeholder="Email" class="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div class="form-control">
+
+                                <input type="text" placeholder="Subject" class="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <textarea class="textarea textarea-primary" placeholder="Your message"></textarea>
+                            </div>
+                            <div class="form-control mt-6">
+                                <button class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -10,7 +10,7 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-170px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 p-10'>
                 <h3 className='text-xl text-primary uppercase py-2'>Appointment</h3>
                 <h2 className='text-3xl text-white py-2'>Make an Appointment</h2>
                 <p className='py-2 text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut praesentium ipsa, repellendus voluptatum ratione, dignissimos excepturi rem saepe illo, necessitatibus quia aspernatur laudantium minus temporibus laboriosam doloribus enim nostrum tenetur! Unde sint itaque provident hic fugiat expedita dolores eum nemo.</p>
